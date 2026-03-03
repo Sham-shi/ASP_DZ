eb641d83d89c1ccd74c6d902b7f54b3554a4f8b6
ShamilShigapov <sshigapov1990@gmail.com>
2026-03-03 23:11:24 +0300

    Дополнил дз 11

---
+++ DZ_11/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace DZ_11
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            var attribute = field?.GetCustomAttribute<DisplayAttribute>();
+            return attribute?.GetName() ?? enumValue.ToString();
+        }
+    }
+}
